4e98f52f18f12acd6e2f3035330b9df6778501e7
Grinchek <warning686@gmail.com>
2023-04-21 20:18:32 +0300

    This exumple show, how to add to vector's element - string variable, using "getline()" for input all line. Also, example  is showing, how to sort(handle) vector of string.

---
+++ Vector_general/Documentation.cpp
@@ -0,0 +1,2 @@
+//For input date of deal use next format "YY.MM.DD", for simple sorting by date.
+//If you want to input aal line to string variale, and than add it to vector - use "getline(cin,"variable_name").
---
+++ Vector_general/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+void main() {
+	vector<string> list;
+	for (int i = 0; i < 4; i++) {
+		string input;
+		getline(cin, input);
+		list.push_back(input);
+	}
+	cout << "==========================================" << endl;
+	for (auto varible : list) {
+		cout << varible << endl;
+	}
+	cout<<"=========================================="<< endl;
+	for (int i = 0; i < 4; i++) {
+		string temp ;
+		for (int j = 0; j < 4; j++) {
+			if (list[i] < list[j]) {
+				temp = list[j];
+				list[j] = list[i];
+				list[i] = temp;
+			}
+		}
+	}
+	for (auto variable : list) {
+		cout <<variable << endl;
+	}
+
+
+}
